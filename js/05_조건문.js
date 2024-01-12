bb4317b3c2eb91fa801dfe2693ae12b820a5002e
ddk9595 <027620@naver.com>
2024-01-12 14:55:52 +0900

    나이 구분하기

--- js/05_조건문.js
+++ js/05_조건문.js
@@ -34,3 +34,56 @@ function check2(){
     }
 
 }
+
+/* -3 ~ 3 사이 난수 만들기 */
+function check3(){
+    const randomNumber = Math.floor(Math.random()*7)-3 ;
+    
+
+    let message = randomNumber + "은/는";
+    
+    if(randomNumber == 0){
+        // message = message +"0 입니다";
+        message += "0입니다." ; //복합 대입 연산자 활용
+    }
+
+    else if(randomNumber > 0){
+        message += "양수입니다.";
+    }
+
+    else { //0도 양수도 아닌 경우 == 음수인 경우
+        message += "음수입니다.";
+    }
+
+
+    alert(message);
+}
+
+
+/* 어린이, 청소년, 성인 구분하기 */
+const inputAge  = document.getElementById("inputAge") ;
+function check4(){
+    
+    //입력한 나이 값을 얻어오기
+    const age = Number(inputAge.value);
+    console.log('age : ', age)
+
+    // if( !(age >= 0 && age<=150)){
+    if(age < 0  || age > 150){
+        alert("잘못 입력하셨습니다.")
+    }
+    
+    else if(age>=0 && age<=13){
+        alert("어린이");
+    }
+
+    else if( age<=19){
+        alert("청소년");
+    }
+
+    else{
+        alert("성인");
+    }
+
+
+}
