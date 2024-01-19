1dc8d4461c592736a4735ce0c6cf9e2e75a8bb23
ddk9595 <027620@naver.com>
2024-01-19 12:01:44 +0900

    함수 선언, 정의, 호출 1. 매개변수(Parameter) 2. 전달인자(Argument) 3. return

--- js/14_입력한색으로변경.js
+++ js/14_입력한색으로변경.js
@@ -1,11 +1,26 @@
-document.getElementById('btn3').addEventListener('click', () => {
-    const containers = document.querySelectorAll('.container3 div');
+// document.getElementById('btn').addEventListener('click', () => {
+//     const containers = document.querySelectorAll('.container div');
+//     containers.forEach((container, index) => {
+//         const input = container.querySelector('input');
+//         const box = container.querySelector('.box');
+//         const color = input.value;
+//         box.style.backgroundColor = color;
+//     });
+// });
 
-    containers.forEach((container, index) => {
-        const input = container.querySelector('input');
-        const box = container.querySelector('.box3');
 
-        const color = input.value;
-        box.style.backgroundColor = color;
-    });
-});
+
+// 요소를 한번에 여러개 얻어오면 배열 형태로 저장됨(배열 형태로 포장)
+// 얻어온 요소를 사용하려면 배열의 index별 요소를 하나씩 꺼내서 사용해야한다!
+// 하나씩 꺼내는 것 : 인덱스를 지정하여 명령
+    /* ex) boxList.style.color = "red" (X)
+        boxList[0].style.color = "blue" */
+
+const boxList = document.querySelectorAll(".box");
+const inputList = document.querySelectorAll(".input");
+
+document.querySelector("#btn").addEventListener("click", function(){
+    for(let i = 0 ; i < boxList.length ; i ++){
+        boxList[i].style.backgroundColor = inputList[i].value;
+    }
+})
