5648eadd60811679803fe4fb013eb0bb0755dd73
ddk9595 <027620@naver.com>
2024-01-22 17:24:33 +0900

    스탑워치 만들기2

--- js/23_스탑와치.js
+++ js/23_스탑와치.js
@@ -1,4 +1,7 @@
 const display         = document.querySelector("#display");
+
+const list = document.querySelectorAll("#display > span");
+
 const startBtn        = document.querySelector("#startBtn");
 const recordBtn       = document.querySelector("#recordBtn");
 const resetBtn        = document.querySelector("#resetBtn");
@@ -17,10 +20,14 @@ startBtn.addEventListener("click", (e)=> {
     curInterval = window.setInterval(()=>{
         // count1 증가
         count++; 
+
+        output(); // 화면에 시간 출력
         
         // document.querySelectorAll("#display > span")[2].innerText = count ;
 
     },10) // 10ms 마다 반복
+
+    e.target.disabled = true ;
 });
 
 /* 시계 출력 함수 */
@@ -34,7 +41,20 @@ function output(){
     second = attatchZero(second);
     msecond = attatchZero(msecond);
 
-    console.log(minute, second, msecond);
+    // console.log(minute, second, msecond);
+
+    // 아웃풋에 입력
+    
+    // minute의 값은 화면에 출력된 분이 다를 경우에만 출력해라
+    if(list[0].innerText != minute) list[0].innerText = minute;
+
+    // second의 값과 화면에 출력된 초가 다를 경우에만 출력
+    if(list[1].innerText != second) list[1].innerText = second;
+
+    list[2].innerText = msecond;
+
+
+
 }
 
 /* 한자리 숫자인 경우 앞에 0 붙여서 반환하는 함수 */
@@ -45,3 +65,17 @@ function attatchZero(num){
     }
     return "" + num;
 }
+
+/* resetBtn 클릭 되었을때 */
+resetBtn.addEventListener("click", ()=>{
+
+    // currInterval 제거
+    window.clearInterval(curInterval);
+
+    // count, 출력된 시간 모두 0으로 바꾸기
+    count=0;
+    output();
+
+    // start버튼 활성화
+    startBtn.disabled = false; // 비활성화를 안하겠다
+});
