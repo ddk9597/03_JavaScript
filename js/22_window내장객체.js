3021cb0aef9eeec45a8da68ad1d6091d2fc1c5d0
ddk9595 <027620@naver.com>
2024-01-23 10:47:18 +0900

    window팝업창 설정

--- js/22_window내장객체.js
+++ js/22_window내장객체.js
@@ -66,3 +66,59 @@ stop3.addEventListener("click", () => {
 
     start3.disabled = false; // start3을 비활성화 하지 않는다. 다시 눌러서 재시작할수 있게
 });
+
+
+// -----------------------------------------------------
+
+
+/* 새 탭에서 열기 */
+
+const openPopup1 = document.querySelector("#openPopup1");
+    openPopup1.addEventListener("click", () => {
+        window.open("23_스탑와치.html");
+
+        // 동일한 방법 : html에서 a 태그 이용하기
+        // <a href = "23_스탑와치.html" target="_blank">스탑와치</a>
+    });
+
+    /* 팝업창으로 열기 */
+
+    const openPopup2 = document.querySelector("#openPopup2");
+    openPopup2.addEventListener("click", () => {
+        window.open("23_스탑와치.html", "_blank", "popup"); // 스탑와치를 비어있는 팝업창에 열어라
+
+
+    });
+
+    /* 팝업창 + 옵션 */
+
+    const openPopup3 = document.querySelector("#openPopup3");
+    openPopup3.addEventListener("click", () => {
+
+        let options = [] ;
+        options.push("width=300px");
+        options.push("height=400px");
+
+        // 팝업창 뜨우는 위치를 지정(메인 모니터에서만 적용됨)
+        options.push("left=200px");
+        options.push("top=100px");
+
+        // 배열명.toString() : "요소, 요소, 요소"  
+        // -> 배열 내 요소를 ,를 구분자로 한 하나의 문자열로 변환
+
+        window.open("23_스탑와치.html", "_blank", "popup"); // 스탑와치를 비어있는 팝업창에 열어라
+
+    });
+
+    /* 팝업창(자식)이 부모창의 값을 읽고 쓰기 */
+    const openPopup4 = document.querySelector("#openPopup4");
+    openPopup4.addEventListener("click", ()=>{
+        const options = [];
+
+        options.push("width = 500")
+        options.push("height = 300")
+        options.push("left = 500")
+        options.push("top = 100")
+
+        window.open("22_팝업창.html", "_blank", options.toString());
+    });
--- js/23_스탑와치.js
+++ js/23_스탑와치.js
@@ -16,7 +16,20 @@ let count = 0; // 1/100초마다 1씩 증가한 시간을 저장할 변수
 // 1    분 : count / 100 / 60
 
 let curInterval; // 인터벌 저장 변수
+
 startBtn.addEventListener("click", (e)=> {
+
+    // 클릭한 버튼에 작성된 내용이 "PAUSE"인 경우
+    if(e.target.textContent == "PAUSE"){
+        e.target.textContent = "START" ; // 버튼 내용  start로 변경
+        window.clearInterval(curInterval); // curInterval 정지
+        return;
+sa
+
+    }
+
+
+    // 클릭한 버튼에 작성된 내용이 "START"인 경우
     curInterval = window.setInterval(()=>{
         // count1 증가
         count++; 
@@ -27,7 +40,8 @@ startBtn.addEventListener("click", (e)=> {
 
     },10) // 10ms 마다 반복
 
-    e.target.disabled = true ;
+    e.target.textContent = "PAUSE"
+
 });
 
 /* 시계 출력 함수 */
@@ -69,7 +83,7 @@ function attatchZero(num){
 /* resetBtn 클릭 되었을때 */
 resetBtn.addEventListener("click", ()=>{
 
-    // currInterval 제거
+    // curInterval 제거
     window.clearInterval(curInterval);
 
     // count, 출력된 시간 모두 0으로 바꾸기
@@ -77,5 +91,40 @@ resetBtn.addEventListener("click", ()=>{
     output();
 
     // start버튼 활성화
-    startBtn.disabled = false; // 비활성화를 안하겠다
+    // startBtn.disabled = false; // 비활성화를 안하겠다
+
+    // start btn 내용을 "START"로 변경
+    startBtn.textContent = "START" ;
+
+    // #recordContainer의 내용을 모두 삭제 ( == 빈 문자열로변경)
+    recordContainer.innerHTML = "";
 });
+
+/* recordBtn 클릭 시 */
+recordBtn.addEventListener("click", ()=> {
+
+    // li 요소 생성
+    const li = document.createElement("li");
+
+    // li 요소 내용으로 #dispaly에 출력된 시간을 얻어와 대입
+    li.innerText = display.innerText ;
+
+    // #recordContainer의 첫 번째 자식으로 li 추가
+    recordContainer.prepend(li);
+});
+
+
+
+
+
+
+
+
+
+/* 요소.textContent 와 요소.innerText */
+
+// textContent 를 이용해 요소에 작성된 내용 얻어오기
+// ->HTML 파일에 작성된 애용 형태(단, 태그 빼고) 그대로 얻어옴 (띄어쓰기, 엔터 모두 포함)
+
+// innerText를 이용해 요소에 작성된 내용 얻어오기
+// -> 브라우저 화면에 보이는 형태(태그 제외)를 얻어옴
